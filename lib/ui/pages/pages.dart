fba2d8ff44c078e60aa84cf04c6781b59127c36a
AgusGunawan96 <mragusgunawan78@gmail.com>
2021-08-31 11:21:22 +0700

    foodmarket-frontend

---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_market/models/models.dart';
+import 'package:food_market/shared/shared.dart';
+import 'package:food_market/ui/widgets/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:supercharged/supercharged.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:get/get.dart';
+
+part 'general_page.dart';
+part 'sign_in_page.dart';
+part 'sign_up_page.dart';
+part 'address_page.dart';
+part 'main_page.dart';
+part 'food_page.dart';
+part 'food_details_page.dart';
+part 'payment_page.dart';
+part 'ilustration_page.dart';
+part 'success_order_page.dart';
+part 'success_sign_up_page.dart';
+part 'order_history_page.dart';
+part 'profile_page.dart';
+
+// class GeneralPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(),
+//     );
+//   }
+// }
